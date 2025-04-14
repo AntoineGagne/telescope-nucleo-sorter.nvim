5a66b4d99a0be45bbaf0d81a769ba95bb4b4e2e4
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-04-14 17:33:51 -0400

    Add a couple of improvements (#1)

--- lua/nucleo/lib.lua
+++ lua/nucleo/lib.lua
@@ -1,7 +1,7 @@
 local uv = vim.uv or vim.loop
 
 local target_shared_lib =
-    string.format('target/release/libnucleo_nvim.%s', uv.os_uname().sysname == 'Darwin' and 'dylib' or 'so')
+  string.format('target/release/libnucleo_nvim.%s', uv.os_uname().sysname == 'Darwin' and 'dylib' or 'so')
 
 local function get_path()
   local bin = vim.api.nvim_get_runtime_file(target_shared_lib, false)[1]
@@ -11,4 +11,5 @@ local function get_path()
   vim.notify_once('nucleo.nvim: only debug build found')
   return (assert(vim.api.nvim_get_runtime_file(target_shared_lib, false)[1], 'lib not found'))
 end
+
 return package.loadlib(get_path(), 'luaopen_nucleo_nvim')()
--- lua/telescope/_extensions/nucleo.lua
+++ lua/telescope/_extensions/nucleo.lua
@@ -1,28 +1,41 @@
-local lib = require 'nucleo.lib'
-local sorters = require 'telescope.sorters'
+local lib = require('nucleo.lib')
+local sorters = require('telescope.sorters')
 
-local matcher = lib.create_matcher()
-
-local function create_sorter()
-    return sorters.Sorter:new {
-        start = function(_, prompt)
-            matcher:set_pattern(vim.trim(prompt))
-        end,
-        discard = true,
-        scoring_function = function(_, _, line)
-            local score = matcher:match(line)
-            if score == 0 then return -1 end
-            return 1 / score
-        end,
-        highlighter = function(_, _, display)
-            local _, hlcols = matcher:match(display)
-            return hlcols
-        end,
-    }
-end
-return require 'telescope'.register_extension {
-    setup = function(_, conf)
-        conf.file_sorter = create_sorter
-        conf.generic_sorter = create_sorter
+local function create_sorter(matcher)
+  return sorters.Sorter:new({
+    start = function(_, prompt)
+      matcher:set_pattern(vim.trim(prompt))
     end,
+    discard = true,
+    scoring_function = function(_, _, line)
+      local score = matcher:match(line)
+      if score == 0 then
+        return -1
+      end
+      return 1 / score
+    end,
+    highlighter = function(_, _, display)
+      local _, hlcols = matcher:match(display)
+      return hlcols
+    end,
+  })
+end
+
+local defaults = {
+  case_mode = 'smart',
+  normalize_mode = 'smart',
 }
+
+return require('telescope').register_extension({
+  setup = function(user_configuration, configuration)
+    local merged = vim.tbl_deep_extend('force', defaults, user_configuration)
+    local matcher = lib.create_matcher(merged)
+
+    configuration.file_sorter = function()
+      return create_sorter(matcher)
+    end
+    configuration.generic_sorter = function()
+      return create_sorter(matcher)
+    end
+  end,
+})
